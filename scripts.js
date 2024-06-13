cfe4964c79a96dfdd92ee80b29bd31907ec71da0
changhyeok <107023878+parannom@users.noreply.github.com>
2024-06-13 13:45:44 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 document.addEventListener('click', function(event) {
     if (event.target.tagName === 'A' && event.target.target === '_blank') {
-        event.preventDefault();
         var url = event.target.href;
         window.interface.MyTest(url);
     }
