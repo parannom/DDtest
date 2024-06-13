fb936f5877e51d8a0d5f44e0f6ef754f53ddc2dd
changhyeok <107023878+parannom@users.noreply.github.com>
2024-06-13 19:04:18 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
-document.addEventListener('click', function(event) {
-    if (event.target.tagName === 'A' && event.target.target === '_blank') {
-        var url = event.target.href;
-        window.interface.MyTest(url);
-    }
-});
+    document.addEventListener('click', function(event) {
+        if (event.target.tagName === 'A' && event.target.target === '_blank') {
+            var url = event.target.href;
+            console.log(url);
+            if (window.ue && window.ue.ueobject) {
+                window.ue.ueobject.launchurl(url).then(() => {
+                    console.log("ue response!");
+                });
+            }
+        }
+    });
